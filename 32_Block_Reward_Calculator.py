f003743d9f5fb023de247e2858736a8a8727c64a
artoiscaplan <artoiscaplan@cdzhongguan.com>
2026-04-10 00:05:18 +0800

    Create 32_Block_Reward_Calculator.py

---
+++ 32_Block_Reward_Calculator.py
@@ -0,0 +1,12 @@
+class BlockReward:
+    def __init__(self, initial=50, halving=210000):
+        self.initial = initial
+        self.halving = halving
+
+    def get_reward(self, height):
+        halvings = height // self.halving
+        return self.initial / (2 ** halvings)
+
+if __name__ == "__main__":
+    br = BlockReward()
+    print(f"区块奖励:{br.get_reward(0)}")
